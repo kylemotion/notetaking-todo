c363affc32a86a392b861ab2d30e2b3d5bd2e7f6
Kyle Harter <99735799+kylemotion@users.noreply.github.com>
2025-05-12 18:02:13 -0400

    started UI building

--- js/main.js
+++ js/main.js
@@ -5,6 +5,6 @@
 const cs = new CSInterface();
 
 document.querySelector('button').addEventListener('click', function(evt){
-    cs.evalScript('sayHello("Clicked!")')})
+    cs.evalScript('sayHello("Notes Submitted!")')})
 
 }())
