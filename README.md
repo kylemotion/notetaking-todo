c1f89f6e36eb48ed0ba6d828ab233cd9c8737cea
Kyle Harter <99735799+kylemotion@users.noreply.github.com>
2025-05-12 17:29:25 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# A repository for testing and learning how to make an extension for After Effects
---
+++ js/main.js
@@ -0,0 +1,10 @@
+(function(){
+
+    'use strict';
+
+const cs = new CSInterface();
+
+document.querySelector('button').addEventListener('click', function(evt){
+    cs.evalScript('sayHello("Clicked!")')})
+
+}())
